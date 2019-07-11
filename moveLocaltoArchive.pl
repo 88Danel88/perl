9301b78059f7f2d5e48ea21e8b86832897464c9a
Danel Tabada <danel.tabada@onsemi.com>
2019-07-12 06:15:22 +0800

    first commit

---
+++ moveLocaltoArchive.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use File::Copy;
+use Time::Piece;
+
+my $dir1 		= "C:/Users/fg8tzv/eclipse-workspace/CIM_Tech_Perl2/directory1"; #will be replaced with LocalStorage
+my $dir2 		= "C:/Users/fg8tzv/eclipse-workspace/CIM_Tech_Perl2/directory2"; #will be replaced with Archive
+my $date 		= localtime->strftime('%m%d%Y'); 								 #string format time , current month day year with no spaces
+my $archive 	= "$dir2/$date"; 												 #folder in dir2 named with current month day year
+my $killDcc     = 'taskkill /IM "Calculator.exe" /F'; 							 #will be replaced with DCC
+my $startDcc   	= 'start calc.exe'; 											 #will be replaced with DCC
+            
+#**************************************************
+#	Sub Routine to kill DCC, open and read dir1,  *
+#	make archive and transfer dir1 files to it    *
+#**************************************************
+
+sub transferFiles {									
+													
+	system($killDcc);								
+													
+	opendir (DIR, $dir1);							
+													
+	while(my $file = readdir DIR){
+		mkdir 	$archive;
+		move	("$dir1/$file" , "$archive/$file");
+		}
+			
+	closedir DIR;
+	
+	system($startDcc)	
+}
+
+#*******************************************************************
+#	Decision making: if dir2 already exist execute transferFiles,  *
+#	Else make dir2 folder first	before transferFiles			   *
+#*******************************************************************
+
+if(-e $dir2){
+	transferFiles();
+	}else{
+	mkdir $dir2;
+	transferFiles();
+}
+
+
+
+
+
+
+
+
